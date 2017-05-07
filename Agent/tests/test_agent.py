c806a8fc7a80908e0420c87c45ab3cbad00ed365
Pawel Kopka <pawel.kopka22@gmail.com>
2017-05-07 22:39:30 +0200

    add first part agent tests

---
+++ Agent/tests/test_agent.py
@@ -0,0 +1,101 @@
+import pytest
+import json
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+
+
+from main import create_app
+
+def json_parser(text: str) -> dict:
+    return json.loads(text)
+
+
+async def test_cpu_percent(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/cpu_percent')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], float)
+
+async def test_cpu_percent_query_interval(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/cpu_percent?interval=0.01')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], float)
+
+
+async def test_cpu_percent_query_percpu(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/cpu_percent?percpu=True')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], list)
+    assert isinstance(data['data'][0], float)
+
+async def test_cpu_percent_query_percpu_interval(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/cpu_percent?percpu=True&interval=0.01')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], list)
+    assert isinstance(data['data'][0], float)
+
+async def test_virtual_memory(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/virtual_memory')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], list)
+    for element in data['data']:
+        assert isinstance(element, (int, float))
+
+async def test_sensors_temperatures(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/sensors_temperatures')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert data.keys() == {'data'}
+    assert isinstance(data['data'], dict)
+    assert isinstance(data['data']['coretemp'], list)
+
+async def test_pid_no_query(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/pid')
+    assert resp.status == 404
+    text = await resp.text()
+    assert 'Missing process name in query'in text
+
+async def test_pid_non_existing_process(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/pid?name=Ty_durniu')
+    assert resp.status == 404
+    text = await resp.text()
+    assert 'Process Ty_durniu not founded'in text
+
+async def test_pid_existing_process(test_client):
+    client = await test_client(create_app)
+    resp = await client.get('/pid?name=python')
+    assert resp.status == 200
+    text = await resp.text()
+    data = json_parser(text)
+    assert isinstance(data, dict)
+    assert isinstance(data['data'], int)
